c27279cd109a57687f6f6cc026e497c351675d2a
Anastasiia Terenteva <terenteva8412@gmail.com>
2020-01-03 16:33:18 +0300

    Initial implementation: - Multiplying matrixes with sizes divisible by the blocksize.

---
+++ block-matrix/block.h
@@ -0,0 +1,80 @@
+//
+// Created by Anastasiia on 02.01.2020.
+//
+
+#ifndef BLOCK_MATRIX_BLOCK_H
+#define BLOCK_MATRIX_BLOCK_H
+
+#include <stdio.h>
+
+/* On my laptop, I can fit 3 matrices of double (8 Bytes) of size
+ * 36x36    into L1 cache (32768 B)
+ * 104x104  into L2 cache (262144 B)
+ * 362x362  into L3 cache (3145728 B)
+ */
+const size_t blocksize = 36;
+
+template<class T>
+class block {
+public:
+    block() : block(blocksize, blocksize) {}
+
+    block(size_t lines, size_t columns) : lines(lines), columns(columns) {
+        bigbody = new T[lines * columns];
+        body = new T *[lines];
+        for (size_t i = 0; i < lines; i++) {
+            body[i] = bigbody + i * columns;
+        }
+    }
+
+    ~block() {
+        delete[] body;
+        delete[] bigbody;
+    }
+
+    T *operator()(size_t line, size_t col) {
+        return body[line] + col;
+    }
+
+    T *operator()(size_t line, size_t col) const {
+        return body[line] + col;
+    }
+
+    bool mul(block const &left, block const &right) {
+        if (left.columns != right.lines) return false;
+        if (lines != left.lines) return false;
+        if (columns != right.columns) return false;
+
+        for (size_t i = 0; i < lines; i++) {
+            for (size_t j = 0; j < columns; j++) {
+                T acc = (T) 0;
+                for (size_t k = 0; k < left.columns; k++) {
+                    acc += *left(i, k) * *right(k, j);
+                }
+                *(*this)(i, j) = acc;
+            }
+        }
+        return true;
+    }
+
+    bool add(block const &left, block const &right) {
+        if (left.columns != right.columns) return false;
+        if (left.lines != right.lines) return false;
+        if (lines != left.lines) return false;
+        if (columns != left.columns) return false;
+
+        for (size_t i = 0; i < lines; i++) {
+            for (size_t j = 0; j < columns; j++) {
+                *(*this)(i, j) = *left(i, j) + *right(i, j);
+            }
+        }
+        return true;
+    }
+
+private:
+    T **body;
+    T *bigbody;
+    size_t lines, columns;
+};
+
+#endif //BLOCK_MATRIX_BLOCK_H
--- block-matrix/blockmatr.h
+++ block-matrix/blockmatr.h
@@ -1,50 +1,62 @@
+#include "block.h"
 
-template<class T> class blockmatrix {
+template<class T>
+class blockmatrix {
+    using block_t = block<T>;
+
 public:
-  blockmatrix(size_t lines, size_t columns): lines(lines), columns(columns) { // subject to change
-    bigbody = new T[lines * columns];
-    body = new T*[lines];
-    for (size_t i = 0; i < lines; i++) {
-      body[i] = bigbody + i*columns;
-    }
-  }
-   
-  ~blockmatrix() { // subject to change
-    delete [] body;
-    delete [] bigbody;
-  }
-   
-  T * operator()(size_t line, size_t col) { // *acc(0,0) = 2.34; *acc(i,j) = *acc(i,j+1);
-    return body[line]+col;
-  }
-   
-  T * operator()(size_t line, size_t col) const { // *acc(0,0) = 2.34; *acc(i,j) = *acc(i,j+1);
-    return body[line]+col;
-  }
-   
-  bool mul(blockmatrix const &left, blockmatrix const &right) {
-    if (left.columns != right.lines) return false;
-    if (lines != left.lines) return false;
-    if (columns != right.columns) return false;
-    for (size_t i = 0; i < lines; i++) {
-      for (size_t j = 0; j < columns; j++) {
-        T acc = (T)0;
-        for (size_t k = 0; k < left.columns; k++) {
-          acc += *left(i,k) * *right(k,j);
+    blockmatrix(size_t lines, size_t columns) : lines(lines), columns(columns) {
+        block_lines = lines / blocksize;
+        block_columns = columns / blocksize;
+
+        bigbody = new block_t[block_lines * block_columns];
+        body = new block_t *[block_lines];
+        for (size_t i = 0; i < block_lines; i++) {
+            body[i] = bigbody + i * block_columns;
         }
-        *(*this)(i,j) = acc;
-      }
     }
-    return true;
-  }
-  
-  bool add(blockmatrix const &left, blockmatrix const &right); 
-  static void tune(size_t arg) { // subject for change
-    ;
-  }
+
+    ~blockmatrix() {
+        delete[] body;
+        delete[] bigbody;
+    }
+
+    T *operator()(size_t line, size_t col) {
+        return (*(body[line / blocksize] + col / blocksize))(line % blocksize, col % blocksize);
+    }
+
+    T *operator()(size_t line, size_t col) const {
+        return (*(body[line / blocksize] + col / blocksize))(line % blocksize, col % blocksize);
+    }
+
+    bool mul(blockmatrix const &left, blockmatrix const &right) {
+        if (left.columns != right.lines) return false;
+        if (lines != left.lines) return false;
+        if (columns != right.columns) return false;
+
+        for (size_t i = 0; i < block_lines; i++) {
+            for (size_t j = 0; j < block_columns; j++) {
+                block_t acc;
+                for (size_t k = 0; k < left.block_columns; k++) {
+                    acc.mul(*left.block(i, k), *right.block(k, j));
+                    this->block(i, j)->add(*this->block(i, j), acc);
+                }
+            }
+        }
+        return true;
+    }
+
 private:
-  T ** body;
-  T *bigbody;
-  size_t lines, columns;
+    block_t **body;
+    block_t *bigbody;
+    size_t lines, columns, block_lines, block_columns;
+
+    block_t *block(size_t block_line, size_t block_col) {
+        return body[block_line] + block_col;
+    }
+
+    block_t *block(size_t block_line, size_t block_col) const {
+        return body[block_line] + block_col;
+    }
 };
 
