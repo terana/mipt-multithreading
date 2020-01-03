bde1b29614345c9f16515f96c1d3a2f0293a391a
Anastasiia Terenteva <terenteva8412@gmail.com>
2020-01-03 22:30:38 +0300

    Copyed the traditional matrix multiplication into a separate class.

--- block-matrix/main.cpp
+++ block-matrix/main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include "blockmatr.h"
 #include <X86intrin.h>
 #include <string>
+
+#include "blockmatr.h"
+#include "matrix.h"
 
 using namespace std;
 
@@ -13,27 +15,45 @@ int main(int argc, char **argv) {
             cy = ay, cx = bx;
 
     blockmatrix<double> a(ay, ax), b(by, bx), c(cy, cx);
+    matrix<double> d(ay, ax), e(by, bx), f(cy, cx);
 
     for (int i = 0; i < ay; i++) {
         for (int j = 0; j < ax; j++) {
             *a(i, j) = i * j;
+            *d(i, j) = i * j;
         }
     }
 
     for (int i = 0; i < by; i++) {
         for (int j = 0; j < bx; j++) {
             *b(i, j) = i * j;
+            *e(i, j) = i * j;
         }
     }
 
-    long long start = __rdtsc();
-    c.mul(a, b);
-    long long end = __rdtsc();
-    double sum = 0;
-    for (int i = 0; i < cy; i++) {
-        for (int j = 0; j < cx; j++) {
-            sum += *c(i, j);
+    {
+        long long start = __rdtsc();
+        c.mul(a, b);
+        long long end = __rdtsc();
+        double sum = 0;
+        for (int i = 0; i < cy; i++) {
+            for (int j = 0; j < cx; j++) {
+                sum += *c(i, j);
+            }
         }
+        printf("blockmatrix: sum=%.0lf time=%.6f\n", sum, (end - start) / 1000000000.);
     }
-    printf("sum=%.0lf time=%.6f\n", sum, (end - start) / 1000000000.);
+
+    {
+        long long start = __rdtsc();
+        f.mul(d, e);
+        long long end = __rdtsc();
+        double sum = 0;
+        for (int i = 0; i < cy; i++) {
+            for (int j = 0; j < cx; j++) {
+                sum += *f(i, j);
+            }
+        }
+        printf("matrix: sum=%.0lf time=%.6f\n", sum, (end - start) / 1000000000.);
+    }
 }
